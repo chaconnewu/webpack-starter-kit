0f9fa1f4fe5716b0e3ac1ee35ad7043c552f70da
Yu Wu <ywu@evernote.com>
2015-07-30 09:33:53 -0700

    Using babel as jsx compiler

--- src/client/app.js
+++ src/client/app.js
@@ -1,4 +1,4 @@
-var React = require('React');
+var React = require('react');
 require('./app.less');
 
 var App = React.createClass({
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
-var ClosureCompilerPlugin = require('webpack-closure-compiler');
+// var ClosureCompilerPlugin = require('webpack-closure-compiler');
+var path = require('path');
 
-module.exports = {
-  entry: './src/client/app.js',
+var config = {
+  entry: path.resolve(__dirname, './src/client/app.js'),
   output: {
+    path: path.resolve(__dirname, './src/client/'),
     filename: './src/client/bundle.js'
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'jsx-loader?harmony' },
+      {
+        test: /\.jsx?$/, // A regexp to test the require path. accepts either js or jsx
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel' // The module to load. "babel" is short for "babel-loader"
+      },
       { test: /\.css$/, loader: 'style!css' },
       { test: /\.less$/, loader: 'style!css!less' },
       { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' } // inline base64 URLs for <=8k images, direct URLs for the rest
@@ -17,6 +23,7 @@ module.exports = {
     'react': 'React'
   },
   plugins: [
-    new ClosureCompilerPlugin()
+    // new ClosureCompilerPlugin()
   ]
 };
+module.exports = config;
